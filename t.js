06289488210a42d4c6a3e77ae0263f7443b21bf5
gonzalezsoriano777 <RAFTB.Gonzalez@Outlook.com>
2018-09-08 17:13:40 +0000

    Prac

---
+++ t.js
@@ -0,0 +1,13 @@
+var hey = {
+    name: hello,
+    color: yellow,
+    height: "50 cm",
+    religion: Christian,
+    
+    
+    
+    hellp: function() {
+        return this.name + " , " + this.religion;
+    }
+    
+};
